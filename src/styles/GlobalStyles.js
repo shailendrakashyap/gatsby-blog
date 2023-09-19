eb789d2d00b5f731b95270ac1592e23ade6be0eb
[shailendrakashyap] <[46565698+shailendrakashyap@users.noreply.github.com]>
2023-09-19 05:21:56 +0000

    19 Sept 2023, 10:51

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,3 +1,25 @@
 import { createGlobalStyle } from "styled-components"
 import NunitoRegular from "../fonts/Nunito-Regular"
 import NunitoBlack from "../fonts/Nunito-Nunito-Black"
+
+export const GlobalStyles = createGlobalStyle`
+    :root{
+        --site-max-width: 1200px;
+    }
+    @font-face {
+        font-family: 'Nunito Regular';
+        src: local('Nunito Regular'), local('NunitoRegular'),
+        url(${NunitoRegular}) format('ttf'),
+        url(${NunitoBlack}) format('ttf');
+        font-weight: 300;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'Nunito Regular';
+        src: local('Nunito Regular'), local('NunitoRegular'),
+
+        url(${NunitoBlack}) format('ttf');
+        font-weight: 300;
+        font-style: normal;
+    }
+`
