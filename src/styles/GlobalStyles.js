0af6e9f64c6745121829857092faad84a050b585
[shailendrakashyap] <[46565698+shailendrakashyap@users.noreply.github.com]>
2023-09-19 05:15:44 +0000

    19 Sept 2023, 10:45

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,1 +1,3 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components"
+import NunitoRegular from "../fonts/Nunito-Regular"
+import NunitoBlack from "../fonts/Nunito-Nunito-Black"
